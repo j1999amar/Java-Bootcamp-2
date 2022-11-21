a2fb6daeec14e7da578f1caf95be73cb1490eb53
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-21 12:32:14 +0530

    largestnumber

--- src/MenuDriven.java
+++ src/MenuDriven.java
@@ -1,16 +1,41 @@
 import java.util.Scanner;
 
 public class MenuDriven {
+    Scanner scanner=new Scanner(System.in);
+
     void menu(){
         System.out.println("Menu");
-        System.out.println("Largest Number ");
-    }
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        MenuDriven menuDriven=new MenuDriven();
-        int menu=scanner.nextInt()
+        System.out.println(" 1. Largest Number ");
+        int menu=scanner.nextInt();
         switch (menu){
             case 1:
+                largestNumber();
         }
+
+    }
+
+    void largestNumber(){
+        System.out.println("Enter the first number");
+        double number1=scanner.nextDouble();
+        System.out.println("Enter the second number");
+        double number2=scanner.nextDouble();
+        System.out.println("Enter the third number");
+        double number3=scanner.nextDouble();
+        if(number1>number2&&number1>number3){
+            System.out.println("Largest Number is "+number1);
+        }else if(number2>number1&&number2>number3){
+            System.out.println("Largest Number is "+number2);
+        }else if(number3>number2&&number3>number1){
+            System.out.println("Largest Number is "+number3);
+        }
+
+
+
+    }
+    public static void main(String[] args) {
+        MenuDriven menuDriven=new MenuDriven();
+        Scanner scanner=new Scanner(System.in);
+        menuDriven.menu();
+
     }
 }
