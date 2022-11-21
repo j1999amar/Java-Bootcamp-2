7091c11fdb1706ce8085b3587adac0277218dc28
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-21 12:42:02 +0530

    smallest number

--- src/MenuDriven.java
+++ src/MenuDriven.java
@@ -6,10 +6,14 @@ public class MenuDriven {
     void menu(){
         System.out.println("Menu");
         System.out.println(" 1. Largest Number ");
+        System.out.println(" 2. Smallest Number ");
+
         int menu=scanner.nextInt();
         switch (menu){
             case 1:
                 largestNumber();
+            case 2:
+                smallestNumber();
         }
 
     }
@@ -28,10 +32,23 @@ public class MenuDriven {
         }else if(number3>number2&&number3>number1){
             System.out.println("Largest Number is "+number3);
         }
-
-
-
     }
+    void smallestNumber(){
+        System.out.println("Enter the first number");
+        double number1=scanner.nextDouble();
+        System.out.println("Enter the second number");
+        double number2=scanner.nextDouble();
+        System.out.println("Enter the third number");
+        double number3=scanner.nextDouble();
+        if(number1<number2&&number1<number3){
+            System.out.println("Smallest Number is "+number1);
+        }else if(number2<number1&&number2<number3){
+            System.out.println("Smallest Number is "+number2);
+        }else if(number3<number2&&number3<number1){
+            System.out.println("Smallest Number is "+number3);
+        }
+    }
+
     public static void main(String[] args) {
         MenuDriven menuDriven=new MenuDriven();
         Scanner scanner=new Scanner(System.in);
