4694ff616ea8e5b853f78c3c1ac418a21a3bc9c4
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-11-21 12:19:46 +0530

    first commit

---
+++ src/MenuDriven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MenuDriven {
+    void menu(){
+        System.out.println("Menu");
+        System.out.println("Largest Number ");
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        MenuDriven menuDriven=new MenuDriven();
+        int menu=scanner.nextInt()
+        switch (menu){
+            case 1:
+        }
+    }
+}
